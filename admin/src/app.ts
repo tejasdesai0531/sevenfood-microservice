fd91bacc3e88382f25d7f6236f322f96802eab19
Tejas Desai <tejadesai0531@gmail.com>
2022-08-24 13:50:47 +0530

    Adds cors to admin and restaurant service

--- admin/src/app.ts
+++ admin/src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 import { json } from 'body-parser';
 import { currentUser, errorHandler, NotFoundError } from '@sevenfood/common';
 import { countryRouter } from './routes/country';
@@ -9,6 +10,7 @@ import { cityRouter } from './routes/city';
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
+app.use(cors())
 
 app.use(currentUser)
 
--- restaurant/src/app.ts
+++ restaurant/src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 import { json } from 'body-parser';
 import { currentUser, errorHandler, NotFoundError } from '@sevenfood/common';
 import { createOutletRouter } from './routes/new'
@@ -8,6 +9,7 @@ import { createOutletRouter } from './routes/new'
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
+app.use(cors())
 
 app.use(currentUser)
 
